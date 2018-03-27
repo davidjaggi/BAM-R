d71ccc5ef481c5abbd7aacb40d6286313e22b815
davidjaggi <davidjaggi@users.noreply.github.com>
2018-03-27 12:40:08 +0100

    Added to MaschineLearning

--- MaschineLearning.R
+++ MaschineLearning.R
@@ -1,7 +1,13 @@
 library(quantmod)
 library(TTR)
+library(xts)
 # Load data
-data <- read.csv('C:/Users/David Jaggi/Google Drive/Eiffeltower Capital/BaData//BA_EURUSD_15min.txt')
+data <- read.csv('C:/Users/David Jaggi/Google Drive/Eiffeltower Capital/BaData//BA_EURUSD_15min.txt', 
+                 header = TRUE, sep = ',')
+
+data$Datetime <- as.POSIXct(paste(data$Date, data$Time), format="%m/%d/%Y %H:%M")
+data <- as.xts(x = data[,-c(1:2,8:9)], order.by = data$Datetime)
+
 
 # Calculate returns
 data$Change <- Delt(data$Close)
@@ -10,12 +16,15 @@ data$Move <- ifelse(data$Change > 0, 1,-1)
 data$VolChange <- Delt(data$Vol)
 
 data$Atr <- ATR(HLC = HLC(data), n = 20)
-data$Atr <- ifelse()
+data$AtrChange <- ifelse(data$Atr - Lag(data$Atr,1) > 0, 1, 0)
 
 data$AtrChange <- ifelse(Lag(data$Atr,1) > 0, Delt(data$Atr), 0)
 
 data <- na.omit(data)
 
+data <- data["2017-01-01/"]
+
+data <- data[,c(1:8,12:13)]
 # Choose indicators Volumechange
 
 # Introduce lag 
