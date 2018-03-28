4ba415641b9e66b89341ffb165eb18b5229608e0
davidjaggi <davidjaggi@users.noreply.github.com>
2018-03-28 09:09:34 +0100

    Added new indicators

--- RandomForest.R
+++ RandomForest.R
@@ -33,42 +33,48 @@ library(randomForestExplainer)
 set.seed(5)
 
 ##Read the stock and index data
-df_stock = read.csv("Data/BA_EURUSD_15min.txt")
+data = read.csv("Data/BA_EURUSD_15min.txt")
 
 ##compute the price change for the stock ans classify as UP/DOWN
-price = df_stock$Close-df_stock$Open
+price = data$Close-data$Open
 class = ifelse(price > 0, "UP","DOWN")
 
 ##compute the technical indicators
 #Force Index Indicator
-forceindex = (df_stock$Close - df_stock$Open)*df_stock$Vol ; forceindex = c(NA,head(forceindex,-1)) ;
+forceindex = (data$Close - data$Open)*data$Vol ; forceindex = c(NA,head(forceindex,-1)) ;
 
 #Buy & Sell signal indicators (williams r% and RSI)
-willR5 = WPR(df_stock[,c("High","Low","Close")], n=5) ; willR5 = c(NA,head(willR5,-1));
-willR10 = WPR(df_stock[,c("High","Low","Close")], n=10) ; willR5 = c(NA,head(willR10,-1));
-willR15 = WPR(df_stock[,c("High","Low","Close")], n=15) ; willR5 = c(NA,head(willR15,-1));
+willR5 = WPR(data[,c("High","Low","Close")], n=5) ; willR5 = c(NA,head(willR5,-1));
+willR10 = WPR(data[,c("High","Low","Close")], n=10) ; willR5 = c(NA,head(willR10,-1));
+willR15 = WPR(data[,c("High","Low","Close")], n=15) ; willR5 = c(NA,head(willR15,-1));
 
-RSI5 = RSI(df_stock$Close, n = 5, matype="WMA"); RSI5 = c(NA,head(RSI5,-1));
-RSI10 = RSI(df_stock$Close, n = 10, matype="WMA"); RSI10 = c(NA,head(RSI10,-1));
-RSI15 = RSI(df_stock$Close, n = 15, matype="WMA"); RSI15 = c(NA,head(RSI15,-1)); 
+RSI5 = RSI(data$Close, n = 5, matype="WMA"); RSI5 = c(NA,head(RSI5,-1));
+RSI10 = RSI(data$Close, n = 10, matype="WMA"); RSI10 = c(NA,head(RSI10,-1));
+RSI15 = RSI(data$Close, n = 15, matype="WMA"); RSI15 = c(NA,head(RSI15,-1)); 
 
 #Price xhange indicators
-ROC5 = ROC(df_stock$Close, n = 5, type = "discrete")*100 ; ROC5 = c(NA,head(ROC5,-1)) ;
-ROC10 = ROC(df_stock$Close, n = 10, type = "discrete")*100 ; ROC10 = c(NA,head(ROC10, -1)) ;
+ROC5 = ROC(data$Close, n = 5, type = "discrete")*100 ; ROC5 = c(NA,head(ROC5,-1)) ;
+ROC10 = ROC(data$Close, n = 10, type = "discrete")*100 ; ROC10 = c(NA,head(ROC10, -1)) ;
 
-MOM5 = momentum(df_stock$Close, n = 5, na.pad = TRUE) ; MOM5 = c(NA,head(MOM5, -1)) ;
-MOM10 = momentum(df_stock$Close, n = 10, na.pad = TRUE) ; MOM10 = c(NA,head(MOM10, -1)) ;
+MOM5 = momentum(data$Close, n = 5, na.pad = TRUE) ; MOM5 = c(NA,head(MOM5, -1)) ;
+MOM10 = momentum(data$Close, n = 10, na.pad = TRUE) ; MOM10 = c(NA,head(MOM10, -1)) ;
 
 #MomIndexes to be inserted
 
 #ATR
-ATR5 = ATR(df_stock[,c("High","Low","Close")], n=5, matype="WMA")[,1] ; ATR5 = c(NA,head(ATR5,-1)) ;
-ATR10 = ATR(df_stock[,c("High","Low","Close")], n=10, matype="WMA")[,1] ; ATR10 = c(NA,head(ATR10,-1)) ;
+ATR5 = ATR(data[,c("High","Low","Close")], n=5, matype="WMA")[,1] ; ATR5 = c(NA,head(ATR5,-1)) ;
+ATR10 = ATR(data[,c("High","Low","Close")], n=10, matype="WMA")[,1] ; ATR10 = c(NA,head(ATR10,-1)) ;
 
 #ATRIndexes to be inserted
 
+# Difference between High and Close
+HC = data$High - data$Close; HC = c(NA, head(HC,-1));
+
+# Difference between Low and Close
+CL = data$Close - data$Low; CL = c(NA, head(CL, -1));
+
 ##Combining all indicators and classes into one dataframe
-dataset = data.frame(class,forceindex,willR5,willR10,willR15,RSI5,RSI10,RSI15,ROC5,ROC10,MOM5,MOM10,ATR5,ATR10)
+dataset = data.frame(class,forceindex,willR5,willR10,willR15,RSI5,RSI10,RSI15,ROC5,ROC10,MOM5,MOM10,ATR5,ATR10,HC,CL)
 dataset = na.omit(dataset)
 
 ##understanding the dataset using descriptive statistics
@@ -80,12 +86,13 @@ cbind(freq=table(y), percentage=prop.table(table(y))*100)
 summary(dataset)
 
 ##visualising the dataset using a correlation matrix
-correlations = cor(dataset[,c(2:14)])
+correlations = cor(dataset[,c(2:ncol(data))])
 print(head(correlations))
 corrplot(correlations, method="number")
 
 ##using randomForest
 fit_rf = randomForest(as.factor(class)~., data = dataset)
+
 importance(fit_rf)
 
 varImpPlot(fit_rf)
